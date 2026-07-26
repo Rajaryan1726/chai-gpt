c0b4b82f1f321d957b97717cb75a258cfb9f5c39
Raj Aryan <rajaryan08161@gmail.com>
2026-07-26 21:09:52 +0530

    UI Part with Streaming Complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
-import {ModeToggle} from "@/components/ui/mode-toggle";
-import { UserButton } from "@clerk/nextjs";
+import { startNewChat } from '@/features/home/actions/start-new-chat'
+import { redirect } from 'next/navigation'
+import React from 'react'
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <ModeToggle />
-      <UserButton />
-    </div>
-  );
+/**
+ * Home page — creates a new chat and redirects to `/c/{id}`.
+ */
+const page = async() => {
+  const conversationId = await startNewChat()
+  
+  
+  redirect(`/c/${conversationId}`)
 }
+
+export default page
